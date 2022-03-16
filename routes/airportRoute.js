4de331595ba66d189e3683bc46728bc75e123890
Suman Kumar Bhadra <mfsi.sumanb@gmail.com>
2022-03-16 19:02:53 +0530

    add: params validator to airport route
    Add params validator to airport create and update routes

--- routes/airportRoute.js
+++ routes/airportRoute.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const airportRouter = Router();
+const { body, validationResult } = require('express-validator');
 
 const { auth } = require("../middlewares/auth");
 
@@ -10,8 +11,46 @@ airportRouter.route("/").get(auth, getAirports);
 airportRouter.route("/report").get(auth, getAirportsReport);
 airportRouter.route("/:airport_id").get(auth, getAirportById);
 airportRouter.route("/:airport_id/transactions").get(auth, getTransactionsByAirport);
-airportRouter.route("/").post(auth, createAirport);
-airportRouter.route("/:airport_id").patch(auth, updateAirport);
+airportRouter.route("/").post(
+    auth,
+    // airport id must be a string
+    body('airport_id').isString(),
+    // airport name must be a string
+    body('airport_name').isString(),
+    // fuel capacity must be a number
+    body('fuel_capacity').isNumeric(),
+    // fuel available must be a number
+    body('fuel_available').isNumeric(),
+    (req, res, next) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error : errors.array()});
+        }
+        next();
+    },
+    createAirport
+);
+airportRouter.route("/:airport_id").patch(
+    auth,
+    // airport id must be a string
+    body('airport_id').isString(),
+    // airport name must be a string
+    body('airport_name').isString(),
+    // fuel capacity must be a number
+    body('fuel_capacity').isNumeric(),
+    // fuel available must be a number
+    body('fuel_available').isNumeric(),
+    (req, res, next) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error : errors.array()});
+        }
+        next();
+    },
+    updateAirport
+);
 airportRouter.route("/:airport_id").delete(auth, deleteAirport);
 
 
