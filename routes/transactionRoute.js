30968bf21ebf949560336a112be6c479673b173a
Suman Kumar Bhadra <mfsi.sumanb@gmail.com>
2022-03-17 11:43:01 +0530

    add: implement params validator in transaction and user routes.
    Body params validator implemented through express validator in user and transaction routes.

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const transactionRouter = Router();
+const { body, validationResult } = require('express-validator');
+
 
 const { auth } = require("../middlewares/auth");
 const { createTransaction, getTransactions, updateTransaction, deleteTransaction, getTransactionById, getTransactionsReport }
@@ -8,8 +10,52 @@ const { createTransaction, getTransactions, updateTransaction, deleteTransaction
 transactionRouter.route("/").get(auth, getTransactions);
 transactionRouter.route("/report").get(auth, getTransactionsReport);
 transactionRouter.route("/:transaction_id").get(auth, getTransactionById);
-transactionRouter.route("/").post(auth, createTransaction);
-transactionRouter.route("/:transaction_id").patch(auth, updateTransaction);
+transactionRouter.route("/").post(
+    auth,
+    // transaction type must be IN/OUT
+    body('transaction_type').if((value) => value == "IN" || value == "OUT"),
+    // airport id must be a string
+    body('airport_id').isString(),
+    // aircraft id must be a string
+    body('aircraft_id').isString(),
+    // quantity must be a number
+    body('quantity').isNumeric(),
+    // transaction id of parent must be a number
+    body('transaction_id_parent').isString(),
+    (req, res, next) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error : errors.array()});
+        }
+        next();
+    },
+    createTransaction
+);
+transactionRouter.route("/:transaction_id").patch(
+    auth,
+    // transaction id must be a string
+    body('transaction_id').isString(),
+    // transaction type must be IN/OUT
+    body('transaction_type').if((value) => value == "IN" || value == "OUT"),
+    // airport id must be a string
+    body('airport_id').isString(),
+    // aircraft id must be a string
+    body('aircraft_id').isString(),
+    // quantity must be a number
+    body('quantity').isNumeric(),
+    // transaction id of parent must be a number
+    body('transaction_id_parent').isString(),
+    (req, res, next) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error : errors.array()});
+        }
+        next();
+    },
+    updateTransaction
+);
 transactionRouter.route("/:transaction_id").delete(auth, deleteTransaction);
 
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,67 @@
 const { Router } = require("express");
 const userRouter = Router();
+const { body, validationResult } = require('express-validator');
 
 const { login, logout, createUser, refresh } = require("../controllers/userController");
 
-userRouter.route("/login").post(login);
-userRouter.route("/logout").post(logout);
-userRouter.route("/refresh").post(refresh);
-userRouter.route("/").post(createUser);
+userRouter.route("/login").post(
+    login,
+    // email
+    body('email').isEmail(),
+    // aircraft id must be a string
+    body('password').isString(),
+    (req, res, next) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error : errors.array()});
+        }
+        next();
+    }
+);
+userRouter.route("/logout").post(
+    logout,
+    // refresh token must be a string
+    body('refreshToken').isString(),
+    (req, res, next) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error : errors.array()});
+        }
+        next();
+    }
+);
+userRouter.route("/refresh").post(
+    refresh,
+    // refresh token must be a string
+    body('refreshToken').isString(),
+    (req, res, next) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error : errors.array()});
+        }
+        next();
+    }
+);
+userRouter.route("/").post(
+    createUser,
+    // name must be a string
+    body('name').isString(),
+    // email
+    body('email').isEmail(),
+    // password must be a string
+    body('password').isString(),
+    (req, res, next) => {
+        // Finds the validation errors in this request and wraps them in an object with handy functions
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error : errors.array()});
+        }
+        next();
+    }
+);
 
 
 
