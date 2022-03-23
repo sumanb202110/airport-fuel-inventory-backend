11bc9c89adbb71c2193d1d37f2fa569f1b85798d
Suman Kumar Bhadra <mfsi.sumanb@gmail.com>
2022-03-23 11:58:56 +0530

    update: comment in airport route
    Update comments in airport route to make it more clear to understand.

--- routes/airportRoute.js
+++ routes/airportRoute.js
@@ -7,10 +7,30 @@ const { auth } = require("../middlewares/auth");
 const { getAirports, createAirport, updateAirport, deleteAirport, getAirportById, getTransactionsByAirport, getAirportsReport }
  = require("../controllers/airportController");
 
+/**
+ * This route is to get all airport details.
+ */
 airportRouter.route("/").get(auth, getAirports);
+
+/**
+ * This route is for airport reports.
+ */
 airportRouter.route("/report").get(auth, getAirportsReport);
+
+/**
+ * This airport is for retriving details of any specific airport by
+ * providing it's id.
+ */
 airportRouter.route("/:airport_id").get(auth, getAirportById);
+
+/**
+ * This route is used to get all transactions of any specific airport.
+ */
 airportRouter.route("/:airport_id/transactions").get(auth, getTransactionsByAirport);
+
+/**
+ * This route is used to create new airport.
+ */
 airportRouter.route("/").post(
     auth,
     // airport id must be a string
@@ -31,6 +51,10 @@ airportRouter.route("/").post(
     },
     createAirport
 );
+
+/**
+ * This route is used to update details of any specific airport details.
+ */
 airportRouter.route("/:airport_id").patch(
     auth,
     // airport id must be a string
@@ -51,6 +75,10 @@ airportRouter.route("/:airport_id").patch(
     },
     updateAirport
 );
+
+/**
+ * This route is used to delete any specific airport by providing it's id.
+ */
 airportRouter.route("/:airport_id").delete(auth, deleteAirport);
 
 
