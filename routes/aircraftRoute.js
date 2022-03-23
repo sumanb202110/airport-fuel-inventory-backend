957c28742c3a8904d10ea28b5421d0104139d0c5
Suman Kumar Bhadra <mfsi.sumanb@gmail.com>
2022-03-23 12:46:33 +0530

    update: aircraft route comment
    Update aircraft route comments for better understanding

--- routes/aircraftRoute.js
+++ routes/aircraftRoute.js
@@ -5,10 +5,19 @@ const { body, validationResult } = require('express-validator');
 const { auth } = require("../middlewares/auth");
 const { createAircraft, getAircrafts, deleteAircraft, updateAircraft, getAircraftById } = require("../controllers/aircraftConroler");
 
+/**
+ * This route retrives all aircrafts details.
+ */
 aircraftRouter.route("/").get(auth, getAircrafts);
 
+/**
+ * This route retrives details of any specific aircraft 
+ */
 aircraftRouter.route("/:aircraft_id").get(auth, getAircraftById);
 
+/**
+ * This route creates new aircraft
+ */
 aircraftRouter.route("/").post(
     auth,
     // aircraft id must be a string
@@ -28,6 +37,9 @@ aircraftRouter.route("/").post(
     createAircraft
 );
 
+/**
+ * This route update details of any specific aircraft
+ */
 aircraftRouter.route("/").patch(
     auth,
     // aircraft id must be a string
@@ -47,6 +59,9 @@ aircraftRouter.route("/").patch(
     updateAircraft
 );
 
+/**
+ * This route delete details of any specific aircraft.
+ */
 aircraftRouter.route("/:aircraft_id").delete(auth, deleteAircraft);
 
 
